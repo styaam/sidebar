0ebc382ab726b07d772ef28c6fa690b0ac3cbfb2
styaam <salhotrastyam@gmail.com>
2022-06-13 14:13:03 +0200

    v1.1

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
-const sidebar = document.getElementById("sideBar");
+const sideBar = document.querySelector(".sideBar");
+const extBar = document.querySelector(".extBar");
+const span = document.querySelector("span");
+const x = document.querySelector(".x");
+const classes = span.classList;
 
-const ye = function () {
-  document.getElementById("sideBar").style.width = "200px";
-};
-
-const back = function () {
-  document.getElementById("sideBar").style.width = "75px";
-};
-
-const pand = function () {
-  if (document.getElementById("sideBar").style.width === "200px") {
-    document.getElementById("sideBar").style.width = "75px";
-  } else {
-    document.getElementById("sideBar").style.width = "200px";
-  }
-};
+x.addEventListener("click", function () {
+  classes.toggle("sideBar");
+});
